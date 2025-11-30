d5f5223d8d89ce5529444f26f32499f19b0cc4e2
Carlos Melegrito <cj@mlgrto.com>
2025-11-30 14:37:45 -0800

    Initial Commit

---
+++ disposable/disposableApp.swift
@@ -0,0 +1,17 @@
+//
+//  disposableApp.swift
+//  disposable
+//
+//  Created by Carlos Melegrito on 30/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct disposableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
